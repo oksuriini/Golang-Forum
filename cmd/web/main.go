3df94fd175e0eb131ebc6227b67efc6bda1ce4f8
Oksuriini <oskari.peiponen@hotmail.com>
2023-05-19 16:01:41 +0300

    modified to placeholder Handlers

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,7 +9,9 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/forum", forum)
-	mux.HandleFunc("/forum/:subject", viewSubject)
+	mux.HandleFunc("/forum/subject", viewSubject)
+	mux.HandleFunc("/forum/subject/thread", viewThread)
+	mux.HandleFunc("/forum/subject/thread", postThread)
 
 	log.Println("Starting Server on :4050")
 	err := http.ListenAndServe(":4050", mux)
