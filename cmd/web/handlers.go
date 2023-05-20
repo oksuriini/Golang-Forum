443ab0e69fa69060292cc72c9645c9ec71070612
Oksuriini <oskari.peiponen@hotmail.com>
2023-05-20 15:18:54 +0300

    added stuff

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,13 +1,38 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+// Add function handlers here
 
 func home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
-	w.Write([]byte("Here is homepage"))
+
+	files := []string{
+		"./ui/html/base.tmpl.html",
+		"./ui/html/partials/footer.tmpl.html",
+		"./ui/html/partials/nav.tmpl.html",
+		"./ui/html/pages/home.tmpl.html",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
 
 func forum(w http.ResponseWriter, r *http.Request) {
@@ -21,28 +46,32 @@ func forum(w http.ResponseWriter, r *http.Request) {
 //func login(w http.ResponseWriter, r *http.Request) {}
 //
 //func logout(w http.ResponseWriter, r *http.Request) {}
-
-func viewSubject(w http.ResponseWriter, r *http.Request) {
-
-	w.Write([]byte("Here is subject folder"))
-}
+//
+//func viewSubject(w http.ResponseWriter, r *http.Request) {
+//	subject := r.URL.Query().Get("sub")
+//	if subject == "" {
+//		http.NotFound(w, r)
+//		return
+//	}
+//	fmt.Fprint(w, fmt.Sprintf("Displaying subject: %s", subject))
+//}
 
 // func viewSubfolder(w http.ResponseWriter, r *http.Request) {}
 //
 // func postSubfolder(w http.ResponseWriter, r *http.Request) {}
 
-func viewThread(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		w.Write([]byte("Method not allowed"))
-	}
-	w.Write([]byte("Here are thread messages"))
-}
-
-func postThread(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		w.Write([]byte("Method not allowed"))
-	}
-	w.Write([]byte("Message posted successfully"))
-}
+//func viewThread(w http.ResponseWriter, r *http.Request) {
+//	if r.Method != http.MethodGet {
+//		w.Header().Set("Allow", http.MethodGet)
+//		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+//	}
+//	w.Write([]byte("Here are thread messages"))
+//}
+//
+//func postThread(w http.ResponseWriter, r *http.Request) {
+//	if r.Method != http.MethodPost {
+//		w.Header().Set("Allow", http.MethodPost)
+//		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+//	}
+//	w.Write([]byte("Message posted successfully"))
+//}
